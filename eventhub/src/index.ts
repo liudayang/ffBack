6aa1994aa1317e48e3b5c7c7b236b133f08b69fd
liudayang <liudayang8110@gmail.com>
2019-10-21 20:43:22 +0800

    inital

---
+++ eventhub/src/index.ts
@@ -0,0 +1,4 @@
+class EventHub {
+
+}
+export default EventHub
---
+++ eventhub/test/index.ts
@@ -0,0 +1,5 @@
+import EventHub from '../src/index'
+
+const eventHub=new EventHub()
+
+console.assert(eventHub instanceof EventHub, 'is object');
